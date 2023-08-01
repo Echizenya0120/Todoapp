ee5c31c0b324147b381b3a3c91e92c57b2a9e42b
Echizenya0120 <s-echizenya@office-navi.co.jp>
2023-08-01 11:43:15 +0900

    <ユーザー名の追加と編集機能の追加をしました。>

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,43 +8,89 @@ use App\Models\Todo;
 
 class TodoController extends Controller
 {
-
-    public function list()
+    /**
+     *　todo一覧取得
+     *
+     */
+    public function list() 
     {
-        $fin = Todo::orderBy('id','asc')->get();
+        //テーブルの情報を取得
+        $todos = Todo::orderBy('id','asc')->get();
 
+        //ユーザー情報を取得
         $user = Auth::user()->name;
 
-        return view('todolist',["fin" =>$fin, "use" =>$user]);
+        //Viewへ情報を送る
+        return view('todolist',["todos" =>$todos, "user" =>$user]);
     }
 
-    public function user()
-    {
-        $user = Auth::user();
-        echo $user->name;
-        
-        return view('createpage',compact($user));
-    }
+   /**
+     *　追加ページ表示
+     *
+     */
 
     public function createpage()
     {
         return view('createpage');
     }
-
-    public function show(Request $request, $id)
-    {
-        $value = $request->session()->get('key');
-    }
-
+    /**
+     * 追加ページ情報を送信
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return todolistページを表示
+     */
     public function creation(Request $request)
     {
+        // 新しくインスタンスを作成
         $todo = new Todo();
+
+        // テーブルに各情報を送信
         $todo->taskname =$request->taskname;
         $todo->assignedperson=$request->assignedperson;
         $todo->time=$request->time;
 
         $todo->save();
 
+        //　todolistページを表示
         return redirect('/todolist');
     }
+    /**
+     *　追加ページ表示
+     *
+     * @return 編集ページにidを渡して表示する。
+     */
+    public function editpage($id)
+    {
+        $edit_Id = Todo::find($id);
+        $user = Auth::user()->name;
+        return view('editpage',["edit_Id" => $edit_Id,"user" =>$user]);
+
+    }
+
+    /**
+     * 編集内容の送信
+     * 
+     * @param \Illuminate\Http\Request $request
+     */
+    public function edition(Request $request)
+    {
+        //テーブルの情報を取得し、アップデートする
+        Todo::find($request->id)->update([
+            "taskname" =>$request->taskname,
+            "assignedperson" =>$request->assignedperson,
+            "time"=>$request->time,
+            "user"=>$request->user,
+
+        ]);
+
+        return redirect('/todolist');
+    }
+   
+     /**
+     *　削除ページ表示
+     *
+     * @return 削除ページにidを渡して表示する。
+     */
+    public function deletepage($id)
+    {}
 }
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,18 @@ Route::post('/creation', [TodoController::class, 'creation']);
 
 Route::get('/create_pag',[TodoController::class, 'user']);
 
+//編集がクリックされたとき
+Route::get('/edit_page/{id}',[TodoController::class, 'editpage']);
+
+//送信がクリックされた時
+Route::post('/edition',[TodoController::class, 'edition']);
+
+//削除がクリックされたとき
+Route::get('/delete_page/{id}',[TodoController::class,'deletepage']);
+
+//送信がクリックされたとき
+Route::post('/delete/{id}',[TodoController::class,'delete']);
+
 
 
 
